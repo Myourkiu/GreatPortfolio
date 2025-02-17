ad65f91b4266929cc58c37d7815a632b78b43e48
Myourkiu <pedrofigsesteves@gmail.com>
2025-02-17 13:15:10 -0300

    fix/overflow-x-auto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Background from "./components/Background";
 export default function Home() {
   return (
     <main className="h-full w-full">
-      <div className="flex flex-col h-[750px] md:h-[850px] gap-20 overflow-x-hidden">
+      <div className="flex flex-col h-[750px] md:h-[850px] gap-20">
         <Background/>
       </div>
     </main>
